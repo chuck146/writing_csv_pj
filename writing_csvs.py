962e5d670f57df1a7eef4f1bdfe468cd1480d02d
Chuck146 <sam98@njit.edu>
2020-03-28 19:33:17 -0400

    Task: "removed code."

--- writing_csvs.py
+++ writing_csvs.py
@@ -1,9 +1,5 @@
 import csv
-import urllib.request as request
-r = request.urlopen(https://raw.githubusercontent.com/jamiec83/Stats-Calculator/commits/master)
-reader = csv.reader(r)
-for line in reader:
-    print(line)
+
 with open('employee_file_dict.csv', mode='w') as employee_file:
     fieldnames = ['name', 'dept', 'birth_month']
     employee_writer = csv.DictWriter(employee_file, fieldnames=fieldnames)
@@ -11,5 +7,5 @@ with open('employee_file_dict.csv', mode='w') as employee_file:
     employee_writer.writeheader()
     employee_writer.writerow({'name':'John Smith', 'dept':'Accounting', 'birth_month':'November'})
     employee_writer.writerow({'name':'Erica Meyers', 'dept':'IT', 'birth_month':'March'})
-    
+
 
